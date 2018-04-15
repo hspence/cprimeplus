0c39312c9e4b35fe6a9a27fb173de3748b2843d3
spence <hspence@163.com>
2018-04-15 16:52:34 +0800

    11-5 的修改

--- 8/11-5.c
+++ 8/11-5.c
@@ -3,24 +3,26 @@
 char get_input();
 
 int main(void){
-    int isflase=1, num=50;
+    int up=100,down=0,isfalse=1; 
     char ch;
     printf("I can guess a number that you think in your heart\n");
-    while(isflase){
+    while(isfalse){
         
-        printf("The number is %d?,it is  bigger than %d ,or smaller?\n",num,num);
-        printf("If it  is %d,,please enter yes\n",num);
+        printf("The number is %d?,it is  bigger than %d ,or smaller?\n",(up+down)/2,(up+down)/2);
+        printf("If it  is %d,,please enter yes\n",(up+down)/2);
         printf("please enter bigger or smaller\n");
         ch=get_input();
     
-        if(ch=='s') num=num/2;
-        if(ch=='b') num=num+num/2;
-        if(ch=='y'){
-            isflase=0;
+        if(ch=='s') up=(up+down)/2;
+        if(ch=='b') down=(up+down)/2; 
+        if(ch=='y'||up==down){
+            isfalse=0;
             break;
     }
 
 }  
+    printf("It is %d? yeah ,I konw i am right..\n",(up+down)/2);
+
     return 0;
 }
 
