4121b36e462c9686178e2dc07a74411e8f1c3d0c
spence <hspence@163.com>
2018-08-22 17:55:42 +0800

    add --all

---
+++ 11/13-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+char* mystrcpy(char*,char*,int);
+
+int main(void)
+{
+    char * sentence1;
+    printf("please enter a sentence:\n");
+    scanf("%s",sentence1);
+
+    while(strcmp(sentence1,"quit"))
+    {
+    
+}
+
+
+    return 0;
+}
