5a609ed9fde68a27fe5ba4ab3cb7c0a728384fdb
spence <15118121067@163.com>
2018-10-04 23:50:12 +0800

    s

--- 13/13-5.c
+++ 13/13-5.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define BUF 4096
 
 int main(int argc,char * argv[])
 {
     FILE * t_fp;
     FILE * s_fp;
-
-    if((t_fp=fopen(argv[1],"a+"))==NULL)
+	char ch;
+	size_t bytes;
+	char temp[BUF];
+	int i;
+	
+    if((t_fp=fopen(argv[1],"ab+"))==NULL)
     {
         fprintf(stderr,"Error in opening file %s ",argv[1]);
         exit(EXIT_FAILURE);
     }
     for(i=2;i<argc;i++)
     {
-        if((s_fp=fopen(argv[i],"r"))) 
-    }
+        if((s_fp=fopen(argv[i],"rb+"))==NULL) 
+    	{
+    		fprintf(stderr,"Error in opening file %s \n",argv[i]);
+    		exit(EXIT_FAILURE);
+		}
+		while((bytes=fread(temp,sizeof(char),BUF,s_fp))>0)
+			fwrite(temp,sizeof(char),bytes,t_fp);
+		
+		printf("Now file %s appended \n",argv[i]);
+	}
+	rewind(t_fp);
+	printf("Now %s file contents:\n",argv[1]);
+	while((ch=getc(t_fp))!=EOF)
+		putc(ch,stdout);
+	puts("Done displaying.");
+	fclose(t_fp);
+	fclose(s_fp);
+	
     return 0;
 }
 
